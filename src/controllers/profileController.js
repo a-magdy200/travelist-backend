fb8e9c1c73ab2b08f34e4f566aa549a54fd34b9e
DinaFH <dinaharb46@gmail.com>
2022-07-02 02:09:20 +0200

    edit profile data

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,16 +1,41 @@
-//const Joi=require('joi');
+const Joi=require('joi');
 const express=require('express');
 
-
+const companies=[
+  {id:1,name:'company1',email:'comapny1@gmail.com',password:'12345',rate:3.5},
+  {id:2,name:'company2',email:'company2@gmail.com',password:'12345',rate:4.8},
+  {id:3,name:'company3',email:'company3@gmail.com',password:'12345',rate:5}
+];
 
  const displayProfile = (req,res,next) => {
-    const companies=[
-        {id:1,name:'company1',email:'comapny1@gmail.com',password:'12345',rate:3.5},
-        {id:2,name:'company2',email:'company2@gmail.com',password:'12345',rate:4.8},
-        {id:3,name:'company3',email:'company3@gmail.com',password:'12345',rate:5}
-    ];
+    
   const company= companies.find(c=>c.id==parseInt(req.params.id));
   if(!company) res.status(404).send('The profile with the given id was not found');
   res.send(company);
 }
-module.exports= {displayProfile};
+
+function validateCompany(company){
+  const schema=Joi.object({
+      name:Joi.string().min(3).required(),
+      email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+
+  });
+  return schema.validate(company);
+}
+
+
+const editProfileData=(req,res,next)=> {
+      const company= companies.find(c=>c.id==parseInt(req.params.id));
+      if(!company) return res.status(404).send('The profile with the given id was not found');
+  
+      const {error}=validateCompany(req.body);
+          res.status(400).send(error.details[0].message);
+          return;
+      }
+  company.name=req.body.name;
+  company.email=req.body.email;
+  res.send(company);
+}
+
+module.exports= {displayProfile,editProfileData};
+
