1e27ea121baec03670506fca0b05b8def5c4ea27
nagwa52 <nagwatalaat52@gmail.com>
2022-07-06 14:30:06 +0200

    add upload with problem

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,18 +1,16 @@
 import { Router } from 'express'
 const router = Router()
 const multer  = require('multer')
-const upload = multer([storage:storage(PROFILES_DIRECTORY)])
-
 // import {viewUserProfile} from "../controllers/user_controller/UserController";
 import { updateUserPassword } from '../controllers/user_controller/UserController'
 import { editUserProfile } from '../controllers/user_controller/UserController'
 import { uploadProfilePicture } from '../controllers/user_controller/UserController'
 import { PROFILES_DIRECTORY } from '../../helpers/constants/directories'
-import { storage } from '../helpers/common/storage-customizer'
-
+import { storage } from '../../helpers/common/storage-customizer'
+const upload = multer({storage:storage(PROFILES_DIRECTORY)})
 
 // router.get('/:id', viewUserProfile);
-router.post('/:id', uploadProfilePicture,upload.single('avatar'))
+router.post('/:id', upload.single('profile_picture'),uploadProfilePicture)
 router.put('/:id', updateUserPassword)
 router.put('/:id', editUserProfile)
 export default router
