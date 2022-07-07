0f5ccba51aa7cabc65183a0004a618c2969ce9ca
HaderMostafa <hadeermostafa.094@gmail.com>
2022-07-07 03:40:01 +0200

    modify register

--- src/controllers/auth/register_handler.ts
+++ src/controllers/auth/register_handler.ts
@@ -1,5 +1,8 @@
 import { AppDataSource } from '../../config/database/data-source'
 import { User } from '../../entities/User.entity'
+import { Traveler } from '../../entities/Traveler.entity'
+import { Company } from '../../entities/Company.entity'
+
 const bcrypt = require('bcrypt')
 import { Request, Response } from 'express'
 import { userValidation } from '../../helpers/validations/user.validation'
@@ -14,20 +17,44 @@ const register = async (req: Request, res: Response, next: any) => {
 
 		// console.log(existedUser);
 
-		if (existedUser == null){
+		if (!existedUser){
 			// const validation = await userValidation.validateAsync(req.body, {
 			// 	abortEarly: false,
 			// })
 
 			const salt = await bcrypt.genSalt(10)
 			const pass = await bcrypt.hash(req.body.password, salt)
+
 			const user = await AppDataSource.manager.insert<User>(User, {
 				name: req.body.name,
 				email: req.body.email,
 				password: pass,
 				address: req.body.address,
+				profile_picture: req.body.profile_picture,
 				type: req.body.type,
 			})
+
+			const userId = user.generatedMaps[0].id;
+			const userType = user.generatedMaps[0].type;
+
+			if(userType == 'traveler'){
+				const traveler = await AppDataSource.manager.insert<Traveler>(Traveler, {
+					national_id: req.body.national_id,
+					gender: req.body.gender,
+					date_of_birth: req.body.date_of_birth,
+					is_guide: req.body.is_guide,
+					user: userId,
+				});
+
+			}else{
+				const company = await AppDataSource.manager.insert<Company>(Company, {
+					description: req.body.description,
+					// rate: req.body.rate,
+					user: userId,
+				});
+			}
+
+			// check that both user and traveler inserted or user and company inserted (revert)	
 
 			jwt.sign({ user }, 'secretkey', { expiresIn: '1h' },(err: any, token: any) => {
 				return res.status(200).json({
@@ -37,6 +64,7 @@ const register = async (req: Request, res: Response, next: any) => {
 					token,
 				})
 			});
+			
 	    }else{
 			return res.status(404).json({
 				success: false,
--- src/helpers/validations/user.validation.ts
+++ src/helpers/validations/user.validation.ts
@@ -5,5 +5,5 @@ export const userValidation = Joi.object().keys({
 	email: Joi.string().email().required(),
 	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
 	address: Joi.string().alphanum().min(3).required(),
-	// profile_picture: Joi.number().min(1).max(5).required(),
+	profile_picture: Joi.string().alphanum().min(5),
 })
