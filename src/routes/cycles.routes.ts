dd6f2320bc25bee7eb2d9f1d99a050515b1d246e
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-07-08 16:16:59 +0200

    Save

--- src/routes/cycles.routes.ts
+++ src/routes/cycles.routes.ts
@@ -1,7 +1,4 @@
 import { Router } from 'express'
-import multer from 'multer'
-import { storage } from '../helpers/common/storage-customizer'
-import { PROGRAMS_DIRECTORY } from '../helpers/constants/directories'
 import {
 	createCycle,
 	deleteCycle,
@@ -11,12 +8,11 @@ import {
 } from '../controllers/cycle'
 
 const router = Router();
-const upload = multer({storage: storage(PROGRAMS_DIRECTORY)});
 
 router.get('/all',showAllCycles );
 router.get('/show/:id',showCycle );
-router.post('/create',upload.none(),createCycle );
-router.put('/update/:id',upload.none(), updateCycle );
+router.post('/create',createCycle );
+router.put('/update/:id', updateCycle );
 router.delete('/delete/:id',deleteCycle );
 
 module.exports = router;
