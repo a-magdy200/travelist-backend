05091a2abca5d8a3c88023ef40a91b5d11b1067b
nagwa52 <nagwatalaat52@gmail.com>
2022-07-08 07:33:40 +0200

    push after company is running

--- src/helpers/functions/returnToken.ts
+++ src/helpers/functions/returnToken.ts
@@ -1,11 +1,31 @@
-import { ValidationError, ValidationErrorItem } from 'joi'
+import { Request, Response } from 'express'
+import jwt_decode from "jwt-decode";
+ const returnToken = (req: Request, res: Response) => {
+	const bearerHeader = req.headers['authorization']
 
-export const formatValidationErrors = (error: ValidationError) => {
-	// TODO:: Handle
-	return {
-		success: false,
-		errors: error?.details
-			? error.details.map((e: ValidationErrorItem) => e.message)
-			: [error.message],
+	if (typeof bearerHeader !== 'undefined') {
+		const Token = bearerHeader.split(' ')[1]
+		//  console.log(Token)
+		return Token
+	} else {
+		res.status(200).json({
+			success: false,
+			message: 'Error! you do not have authorization.',
+		})
 	}
 }
+ const returnId = (req: Request, res: Response) => {
+	const Token = returnToken(req,res)
+	if (Token !==undefined){
+		var decoded :any = jwt_decode(Token);
+	}
+	if(decoded){
+		var userId = decoded.user.id
+		return userId
+	}
+	else{
+		return null
+	}
+	
+ }
+ export{returnToken,returnId}
--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
-import { listCompanies } from '../controllers/CompanyController'
-//import {displayByCompany} from '../controllers/CompanyController';
-import { editCompanyProfile } from '../controllers/CompanyController'
-import { viewCompanyProfile } from '../controllers/CompanyController'
+import { listCompanies } from '../controllers/companies/CompanyController'
+import { editCompanyProfile,uploadCoverPicture,viewCompanyProfile} from '../controllers/companies/CompanyController'
+import { storage } from "../helpers/common/storage-customizer";
+import { PROFILES_DIRECTORY } from "../helpers/constants/directories";
+import { isAuthenticated } from '../middlewares/isAuthenticated'
+const multer  = require('multer')
+const upload = multer({storage:storage(PROFILES_DIRECTORY)})
 
 const router = Router()
 
-router.get('/', listCompanies)
-//router.get('/:id',displayByCompany);
-router.put('/:id', editCompanyProfile)
-router.get('/:id', viewCompanyProfile)
+router.get('/', isAuthenticated,listCompanies)
+router.put('/:id', editCompanyProfile,isAuthenticated)
+ router.get('/:id', viewCompanyProfile)
+router.post('/:id', upload.single('cover_picture'),uploadCoverPicture,isAuthenticated)
 module.exports = router
