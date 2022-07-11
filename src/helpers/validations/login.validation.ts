caf2084361da90f45ef48ffbdfe10a1a447b413e
nagwa52 <nagwatalaat52@gmail.com>
2022-07-11 19:27:03 +0200

    change validation

--- src/helpers/validations/login.validation.ts
+++ src/helpers/validations/login.validation.ts
@@ -2,5 +2,5 @@ import Joi from 'joi'
 
 export const loginValidation = Joi.object().keys({
 	email: Joi.string().email().required(),
-	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]')).required(),
 })
