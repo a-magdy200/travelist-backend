eeba0977cddb1dd1351a8f66bef3fa3a305b7643
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-07-19 16:30:40 +0200

    Added login/register

--- src/helpers/validations/user.validation.ts
+++ src/helpers/validations/user.validation.ts
@@ -1,9 +1,9 @@
 import Joi from 'joi'
 
 export const userValidation = Joi.object().keys({
-	name: Joi.string().alphanum().min(3).required(),
+	name: Joi.string().min(3).required(),
 	email: Joi.string().email().required(),
-	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]')).required(),
-	address: Joi.string().alphanum().min(3).required(),
-	type: Joi.string().valid('traveler', 'company').required(),
+	password: Joi.string().min(6).required(),
+	address: Joi.string().min(3).required(),
+	type: Joi.string().valid('traveler', 'company', 'admin').required(),
 })
