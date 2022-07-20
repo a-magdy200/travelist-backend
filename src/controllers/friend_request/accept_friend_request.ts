2a95fef4e563dfd9d283e87cf7d799c3930a00bf
DinaFH <dinaharb46@gmail.com>
2022-07-20 21:36:15 +0200

    tying to fix user id problem

---
+++ src/controllers/friend_request/accept_friend_request.ts
@@ -0,0 +1,78 @@
+import { FriendRequest } from '../../entities/FriendRequest.entity'
+import { Traveler } from '../../entities/Traveler.entity'
+import { Request, Response } from 'express'
+import { AppDataSource } from '../../config/database/data-source'
+import { sendErrorResponse } from '../../helpers/responses/sendErrorResponse'
+import { sendSuccessResponse } from '../../helpers/responses/sendSuccessResponse'
+import { sendNotFoundResponse } from '../../helpers/responses/404.response'
+import { StatusCodes } from '../../helpers/constants/statusCodes'
+import { FriendRequestStatusType } from '../../helpers/types/friendRequestStatus.type'
+import { FriendRequestStatusEnum } from '../../helpers/enums/friendRequestStatus.enum'
+import { TravelerFriends } from '../../entities/TravelerFriend.entity'
+import { getUserIdFromToken } from '../../helpers/functions/getUserIdFromToken'
+
+
+const acceptFriendRequest = async (req: Request, res: Response) => {
+	const oppsiteTravelerId: number | undefined = +req.params.id
+	const currentUserId: number = getUserIdFromToken(req)
+	console.log(currentUserId)
+	const currentTraveler = await AppDataSource.getRepository(Traveler).findOne({
+		where: {
+			userId: currentUserId,
+		},
+	})
+	if (currentTraveler && currentTraveler.id != oppsiteTravelerId) {
+		console.log(currentTraveler.id)
+		console.log(oppsiteTravelerId)
+		try {
+			const alreadySentRequest: FriendRequest | null =
+				await AppDataSource.manager.findOne<FriendRequest>(FriendRequest, {
+					where: {
+						sender: { id: oppsiteTravelerId },
+						receiver: { id: currentTraveler.id },
+						status: FriendRequestStatusEnum.PENDING,
+					},
+				})
+
+			if (alreadySentRequest) {
+				alreadySentRequest.status = FriendRequestStatusEnum.APPROVED
+				await alreadySentRequest.save()
+
+				const friend: TravelerFriends =
+					await AppDataSource.manager.create<TravelerFriends>(TravelerFriends, {
+						traveler_sender: { id: oppsiteTravelerId },
+						traveler_receiver: { id: currentTraveler.id },
+					})
+				await AppDataSource.manager.save(friend)
+				if (friend) {
+					console.log('friend is added')
+				} else {
+					console.log('can not add friend')
+				}
+				sendSuccessResponse<TravelerFriends>(res, friend)
+
+				//sendSuccessResponse<FriendRequest>(res, alreadySentRequest)
+			} else {
+				sendErrorResponse(
+					['there is not a request '],
+					res,
+					StatusCodes.FORBIDDEN
+				)
+			}
+		} catch (error: any) {
+			sendErrorResponse(['can not accept request'], res, StatusCodes.NO_CHANGE)
+		}
+	} else {
+		sendErrorResponse(
+			['Can not accept friend request'],
+			res,
+			StatusCodes.FORBIDDEN
+		)
+	}
+}
+
+export {
+	
+	acceptFriendRequest,
+	
+}
--- src/routes/friend_request.routes.ts
+++ src/routes/friend_request.routes.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express'
-import {
-	sendFriendRequest,
-	acceptFriendRequest,
-	rejectFriendRequest,
-    cancelFriendRequest,
-
-} from '../controllers/friend_request/FriendRequestController'
+import {sendFriendRequest} from '../controllers/friend_request/send_friend_request'
+import {acceptFriendRequest} from '../controllers/friend_request/accept_friend_request'
+import {rejectFriendRequest} from '../controllers/friend_request/reject_friend_request'
+import {cancelFriendRequest} from '../controllers/friend_request/cancel_friend_request'
 const router = Router()
 
 router.post('/send/:id', sendFriendRequest)
