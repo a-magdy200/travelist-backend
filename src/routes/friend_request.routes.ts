864d03fd41b28b9397429650befccf50306ba548
DinaFH <dinaharb46@gmail.com>
2022-07-21 04:06:57 +0200

    list received and sent friend requests

--- src/routes/friend_request.routes.ts
+++ src/routes/friend_request.routes.ts
@@ -3,10 +3,14 @@ import {sendFriendRequest} from '../controllers/friend_request/send_friend_reque
 import {acceptFriendRequest} from '../controllers/friend_request/accept_friend_request'
 import {rejectFriendRequest} from '../controllers/friend_request/reject_friend_request'
 import {cancelFriendRequest} from '../controllers/friend_request/cancel_friend_request'
+import {listReceivedRequests} from '../controllers/friend_request/list_friend_requests'
+import {listSentRequests} from '../controllers/friend_request/list_friend_requests'
 const router = Router()
 
 router.post('/send/:id', sendFriendRequest)
-router.post('/accept/:id', acceptFriendRequest)
-router.post('/reject/:id', rejectFriendRequest)
-router.post('/cancel/:id', 	cancelFriendRequest)
+router.put('/accept/:id', acceptFriendRequest)
+router.put('/reject/:id', rejectFriendRequest)
+router.delete('/cancel/:id', 	cancelFriendRequest)
+router.get('/sent', 	listReceivedRequests)
+router.get('/received', 	listSentRequests)
 export default router
