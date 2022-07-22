3d2b2e1960bdccc0337d33f4ea4a8277f614c0f9
HaderMostafa <hadeermostafa.094@gmail.com>
2022-07-22 20:41:56 +0200

    fix remove user from group

--- src/controllers/group/remove-user-group.ts
+++ src/controllers/group/remove-user-group.ts
@@ -1,75 +1,64 @@
-import { Request, Response } from "express";
-import { AppDataSource } from "../../config/database/data-source";
-import { Group } from "../../entities/Group.entity";
-import { User } from "../../entities/User.entity";
-import { StatusCodes } from "../../helpers/constants/statusCodes";
-import { formatValidationErrors } from "../../helpers/functions/formatValidationErrors";
-import { getUserIdFromToken } from "../../helpers/functions/getUserIdFromToken";
-import { sendErrorResponse } from "../../helpers/responses/sendErrorResponse";
-import { sendSuccessResponse } from "../../helpers/responses/sendSuccessResponse";
+import { Request, Response } from 'express'
+import { AppDataSource } from '../../config/database/data-source'
+import { Group } from '../../entities/Group.entity'
+import { User } from '../../entities/User.entity'
+import { StatusCodes } from '../../helpers/constants/statusCodes'
+import { formatValidationErrors } from '../../helpers/functions/formatValidationErrors'
+import { getUserIdFromToken } from '../../helpers/functions/getUserIdFromToken'
+import { sendErrorResponse } from '../../helpers/responses/sendErrorResponse'
+import { sendSuccessResponse } from '../../helpers/responses/sendSuccessResponse'
 
 export const removeUserGroup = async (req: Request, res: Response) => {
+	try {
+		const groupId: number | undefined = +req.params.groupId
+		const userId: number = getUserIdFromToken(req)
+		const group = await AppDataSource.getRepository(Group).findOne({
+			where: {
+				id: groupId,
+			},
+			relations: ['followers'],
+		})
+		const user = await AppDataSource.getRepository(User).findOne({
+			where: {
+				id: userId,
+			},
+			relations: ['groups'],
+		})
 
-  try {
-    const groupId: number | undefined = +req.params.groupId
-    const userId: number = getUserIdFromToken(req)
-    const group = await AppDataSource.getRepository(Group).findOne({
-      where: {
-        id: groupId,
-      },
-      relations: ["followers"],
-    })
-    const user = await AppDataSource.getRepository(User).findOne({
-      where: {
-        id: userId,
-      },
-      relations: ["groups"]
-    })
+		let flag: boolean = false
+		if (user) {
+			user.groups.forEach(function (item) {
+				if (item.id == groupId) {
+					flag = true
+					return
+				}
+			})
+		}
+		console.log(flag)
 
+		if (group && user && flag) {
+			group.followers_count -= 1
 
-    let flag: boolean = false
-    if (user) {
-      user.groups.forEach(function (item) {
-        if (item.id == groupId) {
-          flag = true;
-          return
-        }
-      });
-    }
-    console.log(flag)
+			group.followers = group.followers.filter((follower) => {
+				return follower.id !== userId
+			})
 
-    // console.log(user)
-    // console.log(group)
+			await AppDataSource.manager.save(group)
 
-    if (group && user && flag)
-    {
-      console.log("/////////////////////")
-      // group= group.filter(())
-      // await AppDataSource.manager.delete<Group>(Group, {
-      //   followers
-      // })
-
-      group.followers_count -= 1
-
-      await AppDataSource.manager.save(group)
-
-      sendSuccessResponse<Group>(res, group)
-    }
-    else {
-      const error: any = ['not found']
-      sendErrorResponse(
-        formatValidationErrors(error),
-        res,
-        StatusCodes.NOT_ACCEPTABLE
-      )
-    }
-
-  }
-  catch (error: any) {
-    sendErrorResponse(
-      formatValidationErrors(error),
-      res,
-      StatusCodes.NOT_ACCEPTABLE
-    )
-  }
+			sendSuccessResponse<Group>(res, group)
+		} else {
+			const error: any = ['not found']
+			sendErrorResponse(
+				formatValidationErrors(error),
+				res,
+				StatusCodes.NOT_ACCEPTABLE
+			)
+		}
+	} catch (error: any) {
+		sendErrorResponse(
+			formatValidationErrors(error),
+			res,
+			StatusCodes.NOT_ACCEPTABLE
+		)
+	}
 }
