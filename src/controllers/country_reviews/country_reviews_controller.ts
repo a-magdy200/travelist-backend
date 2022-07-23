d02ad2c5ea0a0b3bb1b86bdc54e10cf3d151a731
HaderMostafa <hadeermostafa.094@gmail.com>
2022-07-23 23:32:42 +0200

    modified country review

--- src/controllers/country_reviews/country_reviews_controller.ts
+++ src/controllers/country_reviews/country_reviews_controller.ts
@@ -9,49 +9,40 @@ import { getUserIdFromToken } from '../../helpers/functions/getUserIdFromToken'
 import { Traveler } from '../../entities/Traveler.entity'
 import { CountryReview } from '../../entities/CountryReview.entity'
 import { countryReviewValidation } from '../../helpers/validations/country-review.validation'
+import { Country } from '../../entities/Country.entity'
 
 const listCountriesReviews = async (req: Request, res: Response) => {
-	try{
-	const countries_reviews: CountryReview[] =
-		await AppDataSource.manager.find<CountryReview>(CountryReview, {
-			relations: ['traveler', 'country'],
-		})
+	try {
+		const countries_reviews: CountryReview[] =
+			await AppDataSource.manager.find<CountryReview>(CountryReview, {
+				relations: ['traveler', 'country'],
+			})
 
-	sendSuccessResponse<CountryReview[]>(res, countries_reviews)
-	}
-	catch (e: any) {
-		sendErrorResponse(
-			formatValidationErrors(e),
-			res,
-			StatusCodes.BAD_REQUEST
-		)
+		sendSuccessResponse<CountryReview[]>(res, countries_reviews)
+	} catch (e: any) {
+		sendErrorResponse(formatValidationErrors(e), res, StatusCodes.BAD_REQUEST)
 	}
 }
 
 const showCountryReviews = async (req: Request, res: Response) => {
 	const country_id: number | undefined = +req.params.id
-      try{
-	const country_reviews: CountryReview[] | null =
-		await AppDataSource.manager.find<CountryReview>(CountryReview, {
-			where: {
-				country: { id: country_id },
-			},
-			relations: ['traveler', 'country'],
-		})
+	try {
+		const country_reviews: CountryReview[] | null =
+			await AppDataSource.manager.find<CountryReview>(CountryReview, {
+				where: {
+					country: { id: country_id },
+				},
+				relations: ['traveler', 'country'],
+			})
 
-	if (country_reviews) {
-		sendSuccessResponse<CountryReview[]>(res, country_reviews)
-	} else {
-		sendNotFoundResponse(res)
+		if (country_reviews) {
+			sendSuccessResponse<CountryReview[]>(res, country_reviews)
+		} else {
+			sendNotFoundResponse(res)
+		}
+	} catch (e: any) {
+		sendErrorResponse(formatValidationErrors(e), res, StatusCodes.BAD_REQUEST)
 	}
-}
-catch (e: any) {
-	sendErrorResponse(
-		formatValidationErrors(e),
-		res,
-		StatusCodes.BAD_REQUEST
-	)
-}
 }
 
 const createCountryReview = async (req: Request, res: Response) => {
@@ -65,40 +56,63 @@ const createCountryReview = async (req: Request, res: Response) => {
 					},
 				})
 
-				const currentTravelerId = currentTravelerUser.id
+			const currentTravelerId = currentTravelerUser.id
 
-				const requestedCountryId = req.body?.countryId
+			const requestedCountryId = req.body?.countryId
 
-				const country_review: CountryReview | null =
-					await AppDataSource.manager.findOneOrFail<CountryReview>(CountryReview, {
+			const country_review: CountryReview | null =
+				await AppDataSource.manager.findOneOrFail<CountryReview>(
+					CountryReview,
+					{
 						where: {
 							traveler: { id: currentTravelerId },
 							country: { id: requestedCountryId },
 						},
+					}
+				)
+
+			if (! country_review) {
+				const validation: CountryReview =
+					await countryReviewValidation.validateAsync(req.body, {
+						abortEarly: false,
 					})
 
-				if (!country_review) {
-					const validation: CountryReview =
-						await countryReviewValidation.validateAsync(req.body, {
-							abortEarly: false,
-						})
+				const countryReview = await AppDataSource.manager.create<CountryReview>(
+					CountryReview,
+					{
+						rating: validation.rating,
+						review: validation.review,
+						countryId: requestedCountryId,
+						travelerId: currentTravelerId,
+					}
+				)
 
-					const countryReview =
-						await AppDataSource.manager.create<CountryReview>(CountryReview, {
-							rating: validation.rating,
-							review: validation.review,
-							countryId: requestedCountryId,
-							travelerId: currentTravelerId,
-						})
-					await AppDataSource.manager.save(countryReview)
+				await AppDataSource.manager.save(countryReview)
 
-					sendSuccessResponse<CountryReview>(res, countryReview)
-				} else {
-					sendNotFoundResponse(res, [
-						'Traveler already has reviewed this country',
-					])
+				// modified total rating
+				const country: Country | null = await AppDataSource.getRepository(
+					Country
+				).findOne({
+					where: {
+						id: requestedCountryId,
+					},
+				})
+
+				if (country) {
+					country.ratings_count += 1
+					country.total_rate += validation.rating
+					country.average_rate = country.total_rate / country.ratings_count
+					await country.save()
 				}
-			
+
+				console.log(country)
+
+				sendSuccessResponse<CountryReview>(res, countryReview)
+			} else {
+				sendNotFoundResponse(res, [
+					'Traveler already has reviewed this country',
+				])
+			}
 		} else {
 			sendNotFoundResponse(res, ['invalid token or user is not authenticated'])
 		}
